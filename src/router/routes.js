52eecbfd2d69f5405e8857b4d01bc45020e3e42e
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-17 16:11:19 -0600

    Create routes for tabs/steps needed to create/deploy website from template

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,54 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/Toolbar.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '/registration',
+        component: () => import('pages/Home.vue'),
+        children: [
+          {
+            name: 'Registration',
+            path: '/registration',
+            component: () => import('components/Registration.vue')
+          },
+          {
+            name: 'LandingCarousel',
+            path: '/landing-carousel',
+            component: () => import('components/LandingCarousel.vue')
+          },
+          {
+            name: 'AboutUs',
+            path: '/about-us',
+            component: () => import('components/AboutUs.vue')
+          },
+          {
+            name: 'Highlights',
+            path: '/highlights',
+            component: () => import('components/Highlights.vue')
+          },
+          {
+            name: 'Team',
+            path: '/team',
+            component: () => import('components/Team.vue')
+          },
+          {
+            name: 'Services',
+            path: '/services',
+            component: () => import('components/Services.vue')
+          },
+          {
+            name: 'Reviews',
+            path: '/reviews',
+            component: () => import('components/Reviews.vue')
+          },
+          {
+            name: 'ContactUs',
+            path: '/contact-us',
+            component: () => import('components/ContactUs.vue')
+          }
+        ]
+      }
     ]
   },
 
