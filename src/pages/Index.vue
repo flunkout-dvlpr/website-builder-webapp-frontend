68de4d454985a6d09ca058bdf4ad2b72bf1e01eb
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-17 16:16:51 -0600

    Use q-tabs/q-router-tab to create tabs/sections for each step needed to deploy/create a website from Clean Code template repos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+ <q-page class="row flex-center">
+    <q-card class="col-8 bg-brand-grey-1 shadow-14">
+      <q-card-section align="center" class="bg-brand-grey shadow-14">
+        <q-img
+          src="https://s3.us-east-2.amazonaws.com/www.cleancode.llc/logo-desktop.png"
+          align="center"
+          width="30%"
+        />
+      </q-card-section>
+      <q-tabs
+        switch-indicator
+        class="bg-brand-orange text-grey-3 shadow-14 text-h6"
+        active-color="brand-orange"
+        active-bg-color="brand-grey"
+        align="justify"
+        no-caps
+      >
+        <q-route-tab label="Registration"  :to="{ name: 'Registration' }" />
+        <q-route-tab label="Landing Carousel" :to="{ name: 'LandingCarousel' }" />
+        <q-route-tab label="About Us" :to="{ name: 'AboutUs' }" />
+        <q-route-tab label="Highlights" :to="{ name: 'Highlights' }" />
+        <q-route-tab label="Team" :to="{ name: 'Team' }" />
+        <q-route-tab label="Services" :to="{ name: 'Services' }" />
+        <q-route-tab label="Reviews" :to="{ name: 'Reviews' }" />
+        <q-route-tab label="Contact Us" :to="{ name: 'ContactUs' }" />
+      </q-tabs>
+      <q-card-section>
+        <router-view/>
+      </q-card-section>
+      <footer class="text-center text-white">
+        <div class="col">
+          <q-icon name="copyright" />
+          <a class="text-white text-caption" target="_blank" href="https://www.cleancode.llc"> All Rights Reserved 2020</a>
+        </div>
+      </footer>
+    </q-card>
+ </q-page>
 </template>
 
 <script>
