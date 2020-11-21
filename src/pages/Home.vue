15ecf77c3b7e4032728bab500ffab6b3bcfd4eaa
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-21 17:07:26 -0600

    Add navigation arrows to select next/previous tab, create method to update route based on clicked navigation arrow

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
  <q-page class="row flex-center">
-    <q-card class="col-8 bg-brand-grey-1 shadow-14">
+    <q-card class="q-ma-xl col-md-8 col-xs-11 bg-brand-grey-1 shadow-14">
       <q-card-section align="center" class="bg-brand-grey shadow-14">
         <q-img
           src="https://s3.us-east-2.amazonaws.com/www.cleancode.llc/logo-desktop.png"
@@ -28,11 +28,21 @@
       <q-card-section>
         <router-view/>
       </q-card-section>
-      <footer class="text-center text-white">
-        <div class="col">
-          <q-icon name="copyright" />
-          <a class="text-white text-caption" target="_blank" href="https://www.cleancode.llc"> All Rights Reserved 2020</a>
-        </div>
+      <footer class="q-ma-md  text-center text-grey-9">
+        <div class="row justify-center full-width">
+          <div class="col-md-2 col-xs-4">
+            <q-btn icon="west" class="full-width bg-brand-orange" @click="selectTab('previous')"/>
+          </div>
+          <div class="col-md-8 col-xs-4">
+            <a class="text-grey-3 text-caption" target="_blank" href="https://www.cleancode.llc">
+              Clean Code LLC<br>
+              ©All Rights Reserved 2020
+            </a>
+          </div>
+          <div class="col-md-2 col-xs-4">
+            <q-btn icon="east" class="full-width bg-brand-orange" @click="selectTab('next')"/>
+          </div>
+      </div>
       </footer>
     </q-card>
  </q-page>
@@ -40,6 +50,69 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    selectTab (event) {
+      var currentRoute = this.$route.name
+      switch (currentRoute) {
+        case 'Registration':
+          if (event === 'next') {
+            this.$router.push({ name: 'LandingCarousel' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'ContactUs' })
+          }
+          break
+        case 'LandingCarousel':
+          if (event === 'next') {
+            this.$router.push({ name: 'AboutUs' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'Registration' })
+          }
+          break
+        case 'AboutUs':
+          if (event === 'next') {
+            this.$router.push({ name: 'Highlights' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'LandingCarousel' })
+          }
+          break
+        case 'Highlights':
+          if (event === 'next') {
+            this.$router.push({ name: 'Team' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'AboutUs' })
+          }
+          break
+        case 'Team':
+          if (event === 'next') {
+            this.$router.push({ name: 'Services' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'Highlights' })
+          }
+          break
+        case 'Services':
+          if (event === 'next') {
+            this.$router.push({ name: 'Reviews' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'Team' })
+          }
+          break
+        case 'Reviews':
+          if (event === 'next') {
+            this.$router.push({ name: 'ContactUs' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'Services' })
+          }
+          break
+        case 'ContactUs':
+          if (event === 'next') {
+            this.$router.push({ name: 'Registration' })
+          } else if (event === 'previous') {
+            this.$router.push({ name: 'Reviews' })
+          }
+          break
+      }
+    }
+  }
 }
 </script>
