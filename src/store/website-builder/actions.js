2130c5297e340f1c15075a5f37fb6b6177051698
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-23 15:06:26 -0600

    Add actions to update state and generate s3 bucket link

--- src/store/website-builder/actions.js
+++ src/store/website-builder/actions.js
@@ -30,9 +30,6 @@ export function register ({ commit }, payload) {
 }
 
 export function updateRegistration ({ state, commit }, payload) {
-  // commit('setBusiness', payload)
-  // commit('setTemplate', payload.db_name)
-  console.log(payload)
   return this._vm.$axios.post('update-registration', payload).then((response) => {
     if (response.data.type === 'success') {
       this._vm.$q.notify({
@@ -43,6 +40,56 @@ export function updateRegistration ({ state, commit }, payload) {
       })
       commit('setBusiness', response.data.payload)
       commit('setTemplate', payload.db_name)
+      return response.data.payload
+    } else {
+      this._vm.$q.notify({
+        color: 'warning',
+        textColor: 'grey-9',
+        icon: 'error',
+        message: response.data.message
+      })
+    }
+  }).catch((err) => {
+    this._vm.$q.notify({
+      color: 'warning',
+      textColor: 'grey-9',
+      icon: 'error',
+      message: err.message
+    })
+  })
+}
+
+export function updateSlides ({ commit }, payload) {
+  commit('setSlides', payload)
+}
+
+export function updateAboutUs ({ commit }, payload) {
+  commit('setAboutUs', payload)
+}
+
+export function updateHighlights ({ commit }, payload) {
+  commit('setHighlights', payload)
+}
+
+export function updateTeamMembers ({ commit }, payload) {
+  commit('setTeamMembers', payload)
+}
+
+export function updateServices ({ commit }, payload) {
+  commit('setServices', payload)
+}
+
+export function updateGooglePlace ({ commit }, payload) {
+  commit('setGooglePlace', payload)
+}
+
+export function updateContactUs ({ commit }, payload) {
+  commit('setContactUs', payload)
+}
+
+export function generateUploadLink ({ state, commit }, payload) {
+  return this._vm.$axios.post('generate-upload-link', payload).then((response) => {
+    if (response.data.type === 'success') {
       return response.data.payload
     } else {
       this._vm.$q.notify({
