c8e915812c2562b0826cd3a71f8e74fd27582389
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-23 15:24:26 -0600

    Add prod api link

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 if (process.env.PROD) {
-  axios.defaults.baseURL = 'PROD API'
+  axios.defaults.baseURL = 'https://d305uupefe.execute-api.us-east-2.amazonaws.com/Prod/'
 } else {
   axios.defaults.baseURL = 'http://localhost:3000/'
 }
