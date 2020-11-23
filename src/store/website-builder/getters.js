a040ee22431c9c449a871c97af6979195b760504
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-23 15:06:49 -0600

    Create getters for state props

--- src/store/website-builder/getters.js
+++ src/store/website-builder/getters.js
@@ -10,3 +10,27 @@ export function template (state) {
 export function slides (state) {
   return state.slides
 }
+
+export function aboutUs (state) {
+  return state.aboutUs
+}
+
+export function highlights (state) {
+  return state.highlights
+}
+
+export function teamMembers (state) {
+  return state.teamMembers
+}
+
+export function services (state) {
+  return state.services
+}
+
+export function googlePlace (state) {
+  return state.googlePlace
+}
+
+export function contactUs (state) {
+  return state.contactUs
+}
