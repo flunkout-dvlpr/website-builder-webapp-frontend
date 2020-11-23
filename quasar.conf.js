bd53b73fec4e5c4aef61ccd9714367466d842769
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-22 21:22:12 -0600

    Add lodash to boot settings

--- quasar.conf.js
+++ quasar.conf.js
@@ -19,8 +19,8 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
-
-      'axios'
+      'axios',
+      'lodash'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
