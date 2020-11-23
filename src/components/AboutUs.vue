de8b380b7dc4267b229ecaf1fc086ac09682c205
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-23 15:01:38 -0600

    Refactor variables and implement/integrate vuex updating/image upload to S3

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -13,7 +13,7 @@
                 square
                 outlined
                 color="brand-yellow"
-                v-model="aboutUs.title"
+                v-model="aboutUsData.title"
                 label="Title"
                 placeholder="Suggestion: Business Name/Slogan/Established"
                 :rules="[val => !!val || 'Please enter a title']"
@@ -26,7 +26,7 @@
                 dark
                 filled
                 clearable
-                v-model="aboutUs.description"
+                v-model="aboutUsData.description"
                 type="textarea"
                 style="max-height: 6rem;"
                 label="Business Description"
@@ -36,17 +36,34 @@
             </div>
           </div>
           <div class="row justify-center full-width q-col-gutter-xs q-mb-xs">
-            <div class="col-md-12 col-xs-12">
+            <div class="col-md-12 col-xs-12" v-if="!aboutUsData.image">
               <q-uploader
                 flat
                 square
-                text-color="grey-3"
+                hide-upload-btn
+                style="width: auto"
                 color="brand-orange"
-                class="text-left bg-brand-grey"
-                url="http://localhost:8082/upload"
                 label="Background Image"
-                style="width: auto;"
+                class="text-left bg-brand-grey"
+                :url="url"
+                ref="fileUploader"
+                @added="getUploadLink"
+                @uploaded="onUploadComplete"
+                @rejected="onUploadFail"
+                :headers="[{'Content-Type': '*'}]"
+                :send-raw="true"
+                max-files="1"
+                max-file-size="10000000"
+                method="PUT"
               />
+            </div>
+            <div class="col-md-12 col-xs-12" v-if="aboutUsData.image">
+              <q-img
+                :src="aboutUsData.image"
+                :ratio="4/3"
+              >
+                <q-btn flat no-caps label="Re-Upload" class="shadow-14 bg-brand-yellow text-grey-9" style="top: 85%;" @click="resetImage"/>
+              </q-img>
             </div>
           </div>
         </div>
@@ -90,7 +107,7 @@
               color="brand-orange"
               text-color="grey-9"
               v-model="currentValue"
-              :max="aboutUs.coreValues.length"
+              :max="aboutUsData.coreValues.length"
             />
           </div>
           <div class="row justify-center full-width q-col-gutter-xs q-mb-xs">
@@ -119,12 +136,14 @@ const coreValue = {
   name: null,
   definition: null
 }
+import { mapGetters, mapActions } from 'vuex'
 import AboutUsPreview from 'components/AboutUsPreview'
 export default {
   name: 'AboutUs',
   data () {
     return {
-      aboutUs: {
+      url: null,
+      aboutUsData: {
         title: null,
         description: null,
         image: null,
@@ -138,23 +157,58 @@ export default {
     }
   },
   computed: {
+    ...mapGetters('builder', ['business', 'template', 'aboutUs']),
     valueInView () {
-      return this.aboutUs.coreValues[this.currentValue - 1]
+      return this.aboutUsData.coreValues[this.currentValue - 1]
     }
   },
   methods: {
+    ...mapActions('builder', ['updateAboutUs', 'generateUploadLink']),
+    getUploadLink () {
+      var body = {
+        id: this.business.id,
+        template: this.template,
+        section: 'about-us',
+        fileName: 'aboutUs-image.png'
+      }
+      this.generateUploadLink(body).then((response) => {
+        this.url = response
+      }).then(() => {
+        this.$refs.fileUploader.upload()
+      })
+    },
+    onUploadFail () {
+      this.$q.notify({
+        color: 'warning',
+        textColor: 'grey-2',
+        icon: 'warning',
+        message: 'File extension not supported or File size is too large'
+      })
+    },
+    onUploadComplete () {
+      this.$q.notify({
+        color: 'positive',
+        textColor: 'grey-9',
+        icon: 'send',
+        message: 'File uploaded successfully'
+      })
+      this.aboutUsData.image = `https://website-builder-webapp-data.s3.us-east-2.amazonaws.com/${this.template}/${this.business.id}/about-us/aboutUs-image.png`
+    },
+    resetImage () {
+      this.aboutUsData.image = null
+    },
     addValue () {
       this.$refs.coreValuesForm.validate().then(success => {
         if (success) {
-          this.aboutUs.coreValues.push({ ...coreValue })
+          this.aboutUsData.coreValues.push({ ...coreValue })
           this.currentValue += 1
         }
       })
     },
     deleteValue () {
-      if (this.aboutUs.coreValues.length > 3) {
+      if (this.aboutUsData.coreValues.length > 3) {
         var idx = this.currentValue - 1
-        this.aboutUs.coreValues.splice(idx, 1)
+        this.aboutUsData.coreValues.splice(idx, 1)
         this.currentValue = 1
       } else {
         this.$q.notify({
@@ -169,9 +223,20 @@ export default {
       this.$q.dialog({
         component: AboutUsPreview,
         parent: this,
-        aboutUs: this.aboutUs
+        aboutUs: this.aboutUsData
       })
+    },
+    setAboutUsData () {
+      this.aboutUsData = this.$lodash.cloneDeep(this.aboutUs)
     }
+  },
+  created () {
+    this.setAboutUsData()
+  },
+  beforeRouteLeave (to, from, next) {
+    console.log('Save Changes?') // Ask user to save changes
+    this.updateAboutUs(this.aboutUsData)
+    next()
   }
 }
 </script>
