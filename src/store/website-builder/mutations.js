19ba07b1b07ee57e70f10c49bdefde3cf6f16efd
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-23 15:07:18 -0600

    Create mutations to modify/update state

--- src/store/website-builder/mutations.js
+++ src/store/website-builder/mutations.js
@@ -10,3 +10,32 @@ export function setBusiness (state, payload) {
 export function setTemplate (state, payload) {
   state.template = payload
 }
+
+export function setSlides (state, payload) {
+  state.slides = payload
+}
+
+export function setAboutUs (state, payload) {
+  state.aboutUs = payload
+}
+
+export function setHighlights (state, payload) {
+  state.highlights = payload
+}
+
+export function setTeamMembers (state, payload) {
+  state.teamMembers = payload
+}
+
+export function setServices (state, payload) {
+  state.services = payload
+}
+
+export function setGooglePlace (state, payload) {
+  state.googlePlace = payload
+  state.reviews = payload.reviews
+}
+
+export function setContactUs (state, payload) {
+  state.contactUs = payload
+}
