75dd547e1347f6e713a7cb3e74be645900655c07
flunkout-dvlpr <info@juliogonzalez.space>
2020-11-22 21:24:35 -0600

    Add base API URL bsed on env prod/dev

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+if (process.env.PROD) {
+  axios.defaults.baseURL = 'PROD API'
+} else {
+  axios.defaults.baseURL = 'http://localhost:3000/'
+}
+
 Vue.prototype.$axios = axios
